8d27bdf623908b8edef97cff3007bff5ce17e0d4
Hercules Konsoulas <dyka3773@gmail.com>
2026-08-09 15:46:08 +0200

    feat: add app version next to the title

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from "@angular/core";
+import { Component, inject } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { AuthService } from "../services/auth";
 import { MatToolbarModule } from "@angular/material/toolbar";
@@ -6,6 +6,7 @@ import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
 import { MatTooltipModule } from "@angular/material/tooltip";
 import { Router } from "@angular/router";
+import portalPackage from "../../package.json";
 
 @Component({
 	selector: "app-root",
@@ -22,6 +23,7 @@ import { Router } from "@angular/router";
 export class App {
 	private router = inject(Router);
 	public auth = inject(AuthService);
+	public appVersion = portalPackage.version;
 
 	async logout() {
 		this.auth.signOut();
